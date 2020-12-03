ba8f1b3d3eb1980f0381150fb02b8e96380f18a0
Radosław Grela <radekg23@o2.pl>
2020-12-03 19:23:29 +0100

    split init_database into generate_data and connect_to_mysql methods

--- competence_project/main.py
+++ competence_project/main.py
@@ -1,16 +1,22 @@
-from database.initDB import init_database
+import time
+
+from database.initDB import connect_to_mysql, reset_database
 from database.repository.HotspotRepository import HotspotRepository
-from database.repository.PersonRepository import PersonRepository
 from service import *
-from service import initialize_hotspots
 
-db_cursor, db = init_database()
 
-hotspots = initialize_hotspots(30)
-HotspotRepository.insert_hotspots(db, db_cursor, hotspots)
+def generate_data(nr_of_hotspots=200, nr_of_persons=100):
+    before = time.time()
+    reset_database(db_cursor, db)  # drop and create database
+    hotspots = initialize_hotspots(nr_of_hotspots)
+    HotspotRepository.insert_hotspots(db, db_cursor, hotspots)
+    persons = initialize_persons(nr_of_persons)
+    PersonRepository.insert_persons(db, db_cursor, persons)
+    generate_traces_for_persons(persons, hotspots, db, db_cursor)
+    after = time.time()
+    print("Data generated in " + str(after - before) + "s.")
 
-persons = initialize_persons(5)
-PersonRepository.insert_persons(db, db_cursor, persons)
+
 ########################################################################################################################
 # TODO czasami po uruchomieniu programu wywala błąd:
 #  mysql.connector.errors.IntegrityError: 1452 (23000): Cannot add or update a child row: a foreign key constraint fails
@@ -19,6 +25,12 @@ PersonRepository.insert_persons(db, db_cursor, persons)
 ########################################################################################################################
 # śmietnik #############################################################################################################
 
+
+# h1000 p200 = 81086 -> 4MB
+# h300  p50 = 20319 ->  1MB
+# h100 p20 = 7967 -> 0.4MB
+
+#
 # for i in HotspotRepository.select_hotspots_by_key(db_cursor, "description", "park"):
 #     print(i)
 # for i in HotspotRepository.select_all_hotspots(db_cursor):
@@ -32,14 +44,13 @@ PersonRepository.insert_persons(db, db_cursor, persons)
 # print(PersonRepository.select_person_by_id(db_cursor, 12))
 
 
-TraceRepository.insert_traces(db, db_cursor, [Trace(1, 1, datetime.datetime(2020, 4, 5, 14, 20, 53), datetime.datetime(2020, 4, 5, 15, 20, 53)),
-                                              Trace(1, 2, datetime.datetime(2020, 5, 6, 13, 30, 13), datetime.datetime(2020, 5, 6, 14, 30, 13)),
-                                              Trace(2, 2, datetime.datetime(2021, 5, 6, 13, 30, 13),
-                                                    datetime.datetime(2021, 5, 7, 13, 30, 13))])
+# TraceRepository.insert_traces(db, db_cursor, [Trace(1, 1, datetime.datetime(2020, 4, 5, 14, 20, 53), datetime.datetime(2020, 4, 5, 15, 20, 53)),
+#                                               Trace(1, 2, datetime.datetime(2020, 5, 6, 13, 30, 13), datetime.datetime(2020, 5, 6, 14, 30, 13)),
+#                                               Trace(2, 2, datetime.datetime(2021, 5, 6, 13, 30, 13),
+#                                                     datetime.datetime(2021, 5, 7, 13, 30, 13))])
 
 
-generate_traces_for_persons(persons, hotspots, db, db_cursor)
-calculate_longest_route(db, db_cursor)
+# calculate_longest_route(db, db_cursor)
 # plt.scatter(x1, y1, c='coral')
 # plt.scatter(x2, y2, c='lightblue')
 # plt.show()
@@ -48,5 +59,10 @@ calculate_longest_route(db, db_cursor)
 #     print(dict1)
 
 ########################################################################################################################
-db.close()
 
+
+if __name__ == "__main__":
+    db_cursor, db = connect_to_mysql()
+    generate_data(20, 1)  # czysci baze i generuje od nowa
+    # import_data_from_files()
+    db.close()
